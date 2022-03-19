552aed1bd2b3f348ec71a4e1b76f9f8cf0cf98f7
Michael Simmons <msimmons86@gmail.com>
2022-03-19 15:23:58 -0700

    commit tree e60fb2e9c7203be35a9a5f81b27986a7f1222565

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 3
+let num4 = 1
+let mod = num4 + num3;
+
+let max = Math.max(1, 3, 20);
+
